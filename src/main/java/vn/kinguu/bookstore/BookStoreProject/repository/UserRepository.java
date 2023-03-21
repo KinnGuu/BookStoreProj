1059ed1e046e5847e9ddfcab43c736cc9aecf2e6
DUYHUYNH\Duy <duyhlk@imt-soft.com>
2023-03-21 09:47:35 +0700

    210323_update1

---
+++ src/main/java/vn/kinguu/bookstore/BookStoreProject/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package vn.kinguu.bookstore.BookStoreProject.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public class UserRepository extends JpaRepository<User, Long> {
+
+}
